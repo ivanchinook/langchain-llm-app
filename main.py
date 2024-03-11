eb7b9e36fb4b63cac1be9a80fffaf4f7ad301c06
ivanchinook <ivanchinook@gmail.com>
2024-03-11 01:22:03 +0100

    chain parameter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #from langchain_community.llms import OpenAI
 from langchain_openai import OpenAI
-from langchain.prompts import PromtTemplate
-from langchain.chains import LLMChains
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,13 +9,15 @@ load_dotenv()
 def generate_pet_name(animal_type):
     llm=OpenAI(temperature=0.7)
 
-    prompt_template_name = PromptTemplate()
-        input_variables=["animal_type"]
+    prompt_template_name = PromptTemplate(
+        input_variables=["animal_type"],
         template="I have a {animal_type} pet name and I want a cool name for it. Suggest me five cool names for my pet."
-    name_chain=LLMChains(llm=llm,prompt=prompt_template_name)
+    )
 
-    respone=name_chain({'animal_type': animal_type})
-    return respone;
+    name_chain=LLMChain(llm=llm,prompt=prompt_template_name)
+
+    response=name_chain({'animal_type': animal_type})
+    return response;
 
 if __name__ == "__main__":
-    print(generate_pet_name())
+    print(generate_pet_name('cat'))
