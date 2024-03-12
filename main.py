f7fdbd016e8c0fdde3243b22b5323d5f689d34b4
ivanchinook <110819767+ivanchinook@users.noreply.github.com>
2024-03-12 22:26:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-#from langchain_community.llms import OpenAI
-from langchain_openai import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from dotenv import load_dotenv
+import langchain_helper as lch
+import streamlit as st
 
-load_dotenv()
+st.title("Pets name generator")
 
-def generate_pet_name(animal_type):
-    llm=OpenAI(temperature=0.7)
-
-    prompt_template_name = PromptTemplate(
-        input_variables=["animal_type"],
-        template="I have a {animal_type} pet name and I want a cool name for it. Suggest me five cool names for my pet."
-    )
-
-    name_chain=LLMChain(llm=llm,prompt=prompt_template_name)
-
-    response=name_chain({'animal_type': animal_type})
-    return response;
-
-if __name__ == "__main__":
-    print(generate_pet_name('cat'))
