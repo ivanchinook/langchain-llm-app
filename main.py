6c54ab3195fd2b7d378f9757a4d41882d37a43d5
ivanchinook <110819767+ivanchinook@users.noreply.github.com>
2024-03-14 07:40:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import streamlit as st
 
 st.title("Pets name generator")
 
+user_animal_type=st.sidebar.selectbox("What is your pet?",("Dog","Cat","Cow","Hamster"))
+
+user_pet_color=st.sidebar.text_area(label=f"What color is your {user_animal_type}?", max_chars=15)
+
+if user_pet_color:
+    response = lch.generate_pet_name(user_animal_type, user_pet_color)
+    st.text(response)
